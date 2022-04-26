4ceacd571e086cb03932d4ac3ff9f382c3e9d385
TripToCodes <iam.somj@gmail.com>
2022-04-26 01:36:32 -0700

    Feat: add auth slice

---
+++ client/src/components/LoadingIndicator.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import Content from "./styles/Content.styled";
+
+const StyledLoadingIndicator = styled(Content)`
+  grid-column: 2 / 14;
+  height: 400px;
+  /* x, y, blur-radius, spread */
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 5000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Spinner = styled(Content)`
+  width: 64px;
+  height: 64px;
+  border: 8px solid;
+  border-color: #000 transparent #555 transparent;
+  border-radius: 50%;
+  animation: spin 1.2s linear infinite;
+`;
+
+function LoadingIndicator() {
+  return (
+    <StyledLoadingIndicator>
+      <Spinner></Spinner>
+    </StyledLoadingIndicator>
+  );
+}
+
+export default LoadingIndicator;
--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+import Content from "../components/styles/Content.styled";
 
-function Mypage() {
-  return <div>Mypage</div>;
-}
+const StyledMyPage = styled(Content)`
+  grid-column: 3 / 13;
+  height: 400px;
+  text-align: center;
+  /* x, y, blur-radius, spread */
+`;
 
-export default Mypage;
+const MyPage = () => {
+  return <StyledMyPage>MyPage Page</StyledMyPage>;
+};
+
+export default MyPage;
