d15764ee16dc2992ff88b1605d9d4a973db55498
nicky0830 <kwaknicky@naver.com>
2022-05-01 20:45:24 +0900

    authHeader

--- client/src/features/auth/authHeader.js
+++ client/src/features/auth/authHeader.js
@@ -1,6 +1,5 @@
 export default function authHeader() {
   const user = JSON.parse(localStorage.getItem("user"));
-  console.log(user);
   if (user && user.newAccessToken) {
     return { Authorization: "Bearer " + user.newAccessToken };
   } else {
