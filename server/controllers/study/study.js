a618d17eb0f53791023cd02d8ba710f8a48c4f50
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2022-05-03 13:19:26 +0900

    chore : patch study apit response

--- server/controllers/study/study.js
+++ server/controllers/study/study.js
@@ -155,11 +155,12 @@ module.exports = {
         closed,
         location,
       });
+      console.log(language.id);
 
       for (let i = 0; i < language.length; i++) {
         await Study_language.create({
           study_id: post.id,
-          language_id: language_id[i],
+          language_id: language[i].id,
         });
       }
 
@@ -182,7 +183,7 @@ module.exports = {
         closed,
         location,
         title,
-        language_id,
+        language,
         loginMethod,
       } = req.body;
 
@@ -229,12 +230,12 @@ module.exports = {
       const study_languageId = [];
       study_languageInfo.forEach((el) => study_languageId.push(el.id));
 
-      if (language_id) {
-        if (language_id.length === study_languageId) {
-          for (let i = 0; i < language_id.length; i++) {
+      if (language) {
+        if (language.length === study_languageId) {
+          for (let i = 0; i < language.length; i++) {
             await Study_language.update(
               {
-                language_id: language_id[i],
+                language_id: language[i].id,
               },
               { where: { id: study_languageId[i] } }
             );
@@ -246,10 +247,10 @@ module.exports = {
             });
           }
 
-          for (let i = 0; i < language_id.length; i++) {
+          for (let i = 0; i < language.length; i++) {
             Study_language.create({
               study_id: id,
-              language_id: language_id[i],
+              language_id: language[i].id,
             });
           }
         }
