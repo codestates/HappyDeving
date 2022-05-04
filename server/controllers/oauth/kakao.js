2ba422434e5a48d664b84625ae6980732bf64ab5
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2022-05-04 13:59:15 +0900

    feat : post login kakao api

--- server/controllers/oauth/kakao.js
+++ server/controllers/oauth/kakao.js
@@ -1,42 +1,87 @@
 require("dotenv").config();
-require("dotenv").config();
+const { User } = require("../../models");
 const axios = require("axios");
+const fetch = require("node-fetch");
+const { generateAccessToken, sendTocookie, generaterefreshToken } = require("../tokenFunctions");
 
 module.exports = {
+  get: async (req, res) => {
+    try {
+      res.redirect(
+        `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.CLIENT_ORIGIN}`
+      );
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json();
+    }
+  },
   post: async (req, res) => {
     try {
-      // const code = await axios.get(
-      //   `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.KAKAO_REST_API_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}`
-      // );
+      const { authorizationCode } = req.body;
+      console.log(authorizationCode);
 
-      // console.log(code);
+      if (!authorizationCode) {
+        return res.status(400).json("bad request");
+      }
 
-      let myCode =
-        "SVEsP_6qQEito7KAYARE7VbhrRXGMQldfJ_Get7lY1iKSpkidPD36GVMl8M0YTEGz7whAwopb7kAAAGAiOYYWQ";
+      const KAKAO_CLIENT_ID = process.env.KAKAO_CLIENT_ID;
+      const KAKAO_CLIENT_SECRET = process.env.KAKAO_CLIENT_SECRET;
+      const redirectUri = process.env.CLIENT_ORIGIN;
+      // const grantType = "authorization_code";
 
-      const kakaoClientId = process.env.KAKAO_REDIRECT_URI;
-      const redirectURI = process.env.KAKAO_REDIRECT_URI;
-      const kakaoClientSecret = process.env.KAKAO_CLIENT_SECRET;
-      const code = myCode;
-
-      const resp = await axios({
+      // authorizationCode로 kakao token 을 받아온다.
+      const resp = await fetch("https://kauth.kakao.com/oauth/token", {
         method: "POST",
-        url: `https://kauth.kakao.com/oauth/token}`,
-        headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+        },
         body: new URLSearchParams({
           grant_type: "authorization_code",
-          client_id: kakaoClientId,
-          client_secret: kakaoClientSecret,
-          redirect_uri: redirectURI,
-          code: code,
+          client_id: KAKAO_CLIENT_ID,
+          client_secret: KAKAO_CLIENT_SECRET,
+          redirect_uri: redirectUri,
+          code: authorizationCode,
         }),
-      });
-      console.log(resp.access_token);
+      }).then((res) => res.json());
+      // const { access_token } = resp.data;
+      console.log(resp);
 
-      if (!code) {
-        return res.status(400).json();
+      const kakaoAccessToken = resp.access_token;
+      const kakaoRefreshToken = resp.refresh_token;
+      const kakaoUserInfo = await fetch(`https://kapi.kakao.com/v2/user/me`, {
+        headers: {
+          Authorization: `Bearer ${kakaoAccessToken}`,
+        },
+      }).then((res) => res.json());
+
+      console.log(kakaoUserInfo);
+
+      let { id } = kakaoUserInfo;
+      const { nickname, thumbnail_image_url } = kakaoUserInfo.kakao_account.profile;
+      console.log(nickname);
+      console.log(thumbnail_image_url);
+      id = String(id);
+
+      const userInfo = await User.findOne({
+        where: { password: `${id}${nickname}`, email: `${id}${nickname}@gmail.com` },
+      });
+
+      if (userInfo) {
+        return res.status(403).json("user already existed");
       }
-      res.send(resp);
+      const newUser = await User.create({
+        username: nickname,
+        image: thumbnail_image_url,
+        password: `${id}${nickname}`,
+        email: `${id}${nickname}@gmail.com`,
+      });
+
+      sendTocookie(res, kakaoAccessToken, kakaoRefreshToken);
+
+      return res.status(201).send({
+        newUser,
+        accessToken: kakaoAccessToken,
+      });
     } catch (err) {
       console.error(err);
       return res.status(500).json();
