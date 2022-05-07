4b848019c68fc14c025190d1dacaa6becde0b17b
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2022-05-07 10:11:51 +0900

    chore : post image function

--- server/controllers/mypage/image.js
+++ server/controllers/mypage/image.js
@@ -1,6 +1,6 @@
 const { User, Study_comment, Study, Language, Location } = require("../../models");
 const { checkAccessToken } = require("../tokenFunctions");
-// const uploads = require("../../uploads");
+const { uploadFile, getFileStream } = require("../../middleware/s3");
 
 module.exports = {
   post: async (req, res) => {
@@ -16,14 +16,19 @@ module.exports = {
         return res.status(401).json("wrong req params");
       }
 
-      console.log("req.body=========", req.body);
+      const file = req.file;
+      console.log("file=============", file);
+      const uploadImg = await uploadFile(file);
 
-      // console.log(
-      //   "file:///Users/hy_mac/codestates/HappyDeving/server/uploads/6c47d75228fa992346e8c175f21a79d8"
-      // );
-      const image = `file:///Users/hy_mac/codestates/HappyDeving/server/uploads/${req.file.filename}`;
+      const image = uploadImg.Location;
 
-      return res.json("ok");
+      console.log(typeof image)
+
+      // const image = await getFileStream(result.Key);
+      // console.log("image=========", image);
+
+      // return res.json({ image: result.Location });
+
       const userInfo = await User.findOne({ where: { id: paramsId } });
 
       await User.update(
