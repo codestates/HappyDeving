625ef73bbc1b9d59dd54e4b378be08606b768ecf
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2022-05-07 10:54:51 +0900

    chore : get user email function

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -49,7 +49,7 @@ module.exports = {
       return res.status(500).json();
     }
   },
-  patch: async (req, res) => {
+  get: async (req, res) => {
     try {
       const user = await User.findOne({ where: { id: req.params.id } });
       if (!user) return res.status(400).send({ message: "Invalid link" });
