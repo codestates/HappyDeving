2f804a7a635f408e627353586aaff2624d1fd491
TripToCodes <iam.somj@gmail.com>
2022-05-08 08:01:50 -0700

    Feat4 : Add google login

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -8,6 +8,10 @@ import LoadingIndicator from "../components/LoadingIndicator";
 import Container from "../components/styles/Container.styled";
 import Content from "../components/styles/Content.styled";
 import axios from "axios";
+import { GoogleLoginApi } from "../api/socialAuth";
+import GoogleLogin from "react-google-login";
+import { GOOGLE_CLIENT_ID } from "../config";
+// import { gapi } from "gapi-script";
 
 const Background = styled(Container)`
   grid-column: 1/ 15;
@@ -131,7 +135,7 @@ function Login() {
   };
 
   const getAccessToken = async (authorizationCode) => {
-    let resp = await axios.post("http://localhost:4000/users/login/kakao", {
+    let resp = await axios.post("https://server.happydeving.com/users/login/kakao", {
       authorizationCode: authorizationCode,
     });
 
@@ -161,18 +165,23 @@ function Login() {
     navigate("/");
   };
 
-  const handleGitSignin = async () => {
-    localStorage.setItem("login", "git");
-    const client_id = "66e9a4ba9dc53441a444";
-    const redirect_url = "http://localhost:3000/callback";
-    const GITHUB_LOGIN_URL = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_url=${redirect_url}`;
-    window.location.assign(GITHUB_LOGIN_URL);
+  // useEffect(() => {
+  //   function googleLogin() {
+  //     gapi.client.init({
+  //       clientId: GOOGLE_CLIENT_ID,
+  //       scope: "",
+  //     });
+  //   }
+  //   gapi.load("client:auth2", googleLogin);
+  // });
+  const handleGoogleLoginFailure = (result) => {
+    alert(`${JSON.stringify(result)}`);
   };
 
-  const handleKakaoSignin = async () => {
-    localStorage.setItem("login", "kakao");
-    const kakaoURI = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=5928412b923165af1772a78c664c4582&redirect_uri=http://localhost:3000`;
-    window.location.assign(kakaoURI);
+  const handleGoogleLogin = async (e) => {
+    // body: {token: googleData.tokenId}
+    const googleTokenId = e.tokenId;
+    await GoogleLoginApi({ idToken: googleTokenId });
   };
 
   if (isLoading) {
@@ -208,17 +217,21 @@ function Login() {
             <ButtonWrap>
               <button type="submit">로그인</button>
             </ButtonWrap>
-
-
             <ButtonWrap>
-              <button onClick={handleGitSignin}>깃허브 로그인</button>
-            </ButtonWrap>
-
-            <ButtonWrap>
-              <button type="button" onClick={handleKakaoSignin}>
+              <button type="button" onClick={socialLoginHandler}>
                 카카오
               </button>
-
+              <GoogleLogin
+                clientId={GOOGLE_CLIENT_ID}
+                buttonText="Google"
+                responseType={"id_token"}
+                onSuccess={handleGoogleLogin}
+                onFailure={handleGoogleLoginFailure}
+                cookiePolicy={"single_host_origin"}
+              ></GoogleLogin>
+              {/* <a type="button" href="http://localhost:4000/users/login/kakao">
+                카카오
+              </a> */}
             </ButtonWrap>
             <AlertBox className="alert-box">{errorMessage}</AlertBox>
           </form>
--- server/controllers/oauth/google.js
+++ server/controllers/oauth/google.js
@@ -1,7 +1,37 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const { User } = require("../../models");
+const { generateAccessToken, sendTocookie, generaterefreshToken } = require("../tokenFunctions");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
 module.exports = {
-  post: (req, res) => {
+  post: async (req, res) => {
     try {
-      res.send("users google post ok");
+      const { idToken } = req.body;
+      const ticket = await client.verifyIdToken({
+        idToken: idToken,
+        audience: process.env.GOOGLE_CLIENT_ID,
+      });
+      const payload = ticket.getPayload();
+      console.log("googleLogin payload", payload);
+      const email = payload.email + "-Google";
+      const username = payload.name + String(Math.random()).slice(2, 8);
+      // const password = process.env.SOCIAL_LOGIN_PASSWORD;
+      const image = payload.picture;
+
+      let userInfo = await User.findOne({ email });
+      if (!userInfo) {
+        userInfo = await User.create({ email, username, password: `${email}${username}`, image });
+      }
+      const accessToken = generateAccessToken(userInfo, "accessToken");
+      const refreshToken = generaterefreshToken(userInfo, "refreshToken");
+      sendTocookie(res, accessToken, refreshToken);
+
+      return res.json({
+        userInfo: userInfo,
+        accessToken: accessToken,
+      });
     } catch (err) {
       console.error(err);
       return res.status(500).json();
