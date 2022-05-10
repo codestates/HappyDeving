5edaa9651a970268a0d1a6eda6639010c245a0eb
TripToCodes <iam.somj@gmail.com>
2022-05-10 01:36:43 -0700

    Feat 192 : Responsive landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
-import { gitSignin, kakaoSignin } from "./features/user/userSlice";
-import { useDispatch } from "react-redux";
+import React from "react";
+// import { gitSignin, kakaoSignin } from "./features/user/userSlice";
+// import { useDispatch } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Container from "../src/components/styles/Container.styled";
 import Header from "../src/components/styles/Header.styled";
 import { Search } from "../src/components/styles/Search.styled";
-import Footer from "../src/components/styles/Footer.styled";
+// import Footer from "../src/components/styles/Footer.styled";
 import Landing from "./pages/Landing";
 import Write from "././components/styles/WriteStudyDesc.styled";
 import Map from "././components/styles/Map.styled";
@@ -22,7 +22,12 @@ import "./App.css";
 import "./static/fonts/font.css";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
-import axios from "axios";
+import Slider from "./components/Slider";
+// import axios from "axios";
+import landing_01 from "../src/assets/AdobeStock_434132331.jpeg";
+import landing_02 from "../src/assets/AdobeStock_340974671.jpeg";
+import landing_03 from "../src/assets/AdobeStock_347708874.jpeg";
+import "./App.css";
 
 function App() {
   const theme = {
@@ -46,30 +51,28 @@ function App() {
     font: {},
   };
 
+  // const getAccessToken = async (authorizationCode) => {
+  //   let resp = await axios.post("https://server.happydeving.com/users/login/kakao", {
+  //     authorizationCode: authorizationCode,
+  //   });
+  //   console.log(resp);
+  // };
 
-  const getAccessToken = async (authorizationCode) => {
-    let resp = await axios.post("https://server.happydeving.com/users/login/kakao", {
-      authorizationCode: authorizationCode,
-    });
-    console.log(resp);
-  };
+  // useEffect(() => {
+  //   const url = new URL(window.location.href);
+  //   console.log(url);
 
-
-  useEffect(() => {
-    const url = new URL(window.location.href);
-    console.log(url);
-
-    const authorizationCode = url.searchParams.get("code");
-    if (authorizationCode) {
-      if (localStorage.getItem("login") === "git") {
-        console.log("client auth git", authorizationCode);
-        dispatch(gitSignin(authorizationCode));
-      } else if (localStorage.getItem("login") === "kakao") {
-        console.log("client auth kakao", authorizationCode);
-        dispatch(kakaoSignin(authorizationCode));
-      }
-    }
-  });
+  //   const authorizationCode = url.searchParams.get("code");
+  //   if (authorizationCode) {
+  //     if (localStorage.getItem("login") === "git") {
+  //       console.log("client auth git", authorizationCode);
+  //       dispatch(gitSignin(authorizationCode));
+  //     } else if (localStorage.getItem("login") === "kakao") {
+  //       console.log("client auth kakao", authorizationCode);
+  //       dispatch(kakaoSignin(authorizationCode));
+  //     }
+  //   }
+  // });
 
   return (
     <Router>
@@ -86,8 +89,19 @@ function App() {
               exact
               element={
                 <>
-                  <Search />
-                  <Landing />
+                  {/* <Search /> */}
+                  <Landing imageSrc={landing_01} />
+                  <Slider
+                    imageSrc={landing_02}
+                    title={`" 위치 기반 검색 "`}
+                    subtitle={"당신의 주변에서 일어나고 있는 놀라운 프로젝트를 찾아보세요."}
+                  />
+                  <Slider
+                    imageSrc={landing_03}
+                    title={`" 같이의 가치 "`}
+                    subtitle={"당신의 드림 프로젝트, 함께 하면 현실이 됩니다."}
+                    flipped={true}
+                  />
                 </>
               }
             />
@@ -119,7 +133,7 @@ function App() {
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
           </Routes>
-          <Footer />
+          {/* <Footer /> */}
         </Container>
       </ThemeProvider>
     </Router>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,18 +1,58 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import styled from "styled-components";
-import Content from "../components/styles/Content.styled";
+// import Content from "../components/styles/Content.styled";
 
-const StyledLanding = styled(Content)`
-  grid-column: 2 / 14;
-  height: 400px;
-  text-align: center;
+const StyledLanding = styled.div`
+  grid-column: 3 / 13;
   position: relative;
-  z-index: 0;
-  /* x, y, blur-radius, spread */
+  width: 100%;
+  height: 100vh;
+  transition: 2s;
+  // 태블릿
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    grid-column: 2 / 14;
+    transition: 2s;
+  }
+  // 모바일
+  @media screen and (max-width: 767px) {
+    grid-column: 1 / 15;
+    transition: 2s;
+  }
+`;
+const LandingImage = styled.img`
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+  position: absolute;
+`;
+const LandingTitle = styled.h1`
+  position: absolute;
+  background-color: rgb(236, 236, 236);
+  border: 5px solid rgb(236, 236, 236);
+  top: 20%;
+  color: black;
+  font-size: 2.5rem;
+  padding: 15px;
+  // 태블릿
+  @media screen and (min-width: 768px) and (max-width: 1023px) {
+    transition: 1.5s;
+    font-size: 2rem;
+  }
+  // 모바일
+  @media screen and (max-width: 767px) {
+    font-size: 1.5rem;
+    transition: 1.5s;
+  }
 `;
 
-const Landing = () => {
-  return <StyledLanding>Landing Page</StyledLanding>;
+const Landing = ({ imageSrc }) => {
+  return (
+    <StyledLanding>
+      <LandingImage src={imageSrc} />
+      <LandingTitle>해피데빙으로 협업하자</LandingTitle>
+    </StyledLanding>
+  );
 };
 
 export default Landing;
